f0fe36ed567dba6703e3926e1feb140502a2b4c3
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2023-05-05 19:33:15 +0530

    fw23_0282_day3

--- backend/connection/redis.js
+++ backend/connection/redis.js
@@ -1,6 +1,8 @@
 const redis = require("redis");
+require("dotenv").config();
+let password=process.env.redis_password
 const redisclient = redis.createClient({
-    url: "redis://default:GPpAScAQMw1V91ol66h52md7wILabtag@redis-10253.c301.ap-south-1-1.ec2.cloud.redislabs.com:10253",
+    url: "redis://default:${password}@redis-10253.c301.ap-south-1-1.ec2.cloud.redislabs.com:10253",
 });
 try {
     redisclient.connect();
