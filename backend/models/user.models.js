4d0bf3934db58c1fa8bc7d69fa625f932935dc17
vipullsingh <vipul.kr.vks@gmail.com>
2023-05-08 00:41:50 +0530

    changes

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
+    name: { type: String, required: true },
     logo: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
