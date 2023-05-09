03f3d0f36d015cc9e854ea1d002b6316876bcb8a
vaibhzz101 <vaibhzzsmiley@gmail.com>
2023-05-09 16:27:17 +0530

    update

--- frontend/src/useradmin.js
+++ frontend/src/useradmin.js
@@ -1,38 +1,58 @@
-
-const allUserTable = document.getElementById('allUserTable');
-
+let tbody = document.getElementById("tbody");
     async function fetchUsers() {
-        try {
+        // try {
             const response = await fetch('https://prickly-dove-knickers.cyclic.app/user/allusers');
             const data = await response.json();
+            display(data);
             console.log(data); // Just for debugging purposes
-
+            let alldeletebtn = document.querySelectorAll(".delete");
+            console.log(alldeletebtn);
             // Clear the existing table
-            allUserTable.innerHTML = '';
+            // allUserTable.innerHTML = '';
+            alldeletebtn.forEach((item) => {
 
-            // Add each user to the table
-            data.forEach(user => {
-                const row = document.createElement('tr');
-                const nameCell = document.createElement('td');
-                const emailCell = document.createElement('td');
-                const idCell = document.createElement('td');
-                const deleteCell = document.createElement('td');
+                item.addEventListener("click", async () => {
+                    console.log(item.classList[0]);
+                    let res = await fetch(`https://prickly-dove-knickers.cyclic.app/user/delete/${item.classList[0]}`, {
+                        method: "DELETE",
 
-                nameCell.innerText = user.name;
-                emailCell.innerText = user.email;
-                idCell.innerText = user._id;
-                deleteCell.innerText = 'Delete';
+                    })
+                    if (res.ok) {
+                        let r = res.json();
 
-                deleteCell.addEventListener('click', () => {
-                    deleteUser(user._id);
-                });
+                        alert("delete successfully");
+                        window.location.reload();
 
-                row.append(nameCell, emailCell, idCell, deleteCell);
-                allUserTable.append(row);
-            });
-        } catch (error) {
-            console.error(error);
-        }
+
+                    }
+                })
+            })
+
+  
     }
 
-    fetchUsers();
+fetchUsers();
+    
+function display(data) {
+
+    data.forEach(user => {
+        const row = document.createElement('tr');
+        const nameCell = document.createElement('td');
+        const emailCell = document.createElement('td');
+        const idCell = document.createElement('td');
+        const deleteCell = document.createElement('td');
+        deleteCell.classList.add(user._id, "delete")
+
+        nameCell.innerText = user.name;
+        emailCell.innerText = user.email;
+        idCell.innerText = user._id;
+        deleteCell.innerText = 'Delete';
+
+        // deleteCell.addEventListener('click', () => {
+        //     deleteUser(user._id);
+        // });
+
+        row.append(nameCell, emailCell, idCell, deleteCell);
+        tbody.append(row);
+    });
+}
